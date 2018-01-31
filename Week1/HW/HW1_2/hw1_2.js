b6dd30794cc28c9938cf2915158ffbffe2d76409
RSofiaC <reginascal@gmail.com>
2018-01-31 11:49:04 -0500

    added a validator to the form

--- Week1/HW/HW1_2/hw1_2.js
+++ Week1/HW/HW1_2/hw1_2.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
 
 var app = express();
 
@@ -11,6 +12,30 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req,res,next){
+  res.locals.errors = null
+  next();
+});
+
+//Error formating
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length){
+      formParam+='['+namespace.shift()+']';
+    }
+    // Build your resulting errors however you want! String, object, whatever - it works!
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
+}));
 
 var users = [
   {
@@ -41,13 +66,29 @@ app.get('/', function(req, res){
 });
 
 app.post('/users/add',function(req,res){
-  var newUser = {
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    email: req.body.email
+  req.checkBody('first_name', 'First name is needed').notEmpty();
+  req.checkBody('last_name', 'Gimmie').notEmpty();
+  req.checkBody('email', 'Your info').notEmpty();
+
+  var errors = req.validationErrors();
+
+  if (errors){
+    res.render('index',{
+      title:'Costumers',
+      users: users,
+      errors: errors
+    });
+  }else{
+    var newUser = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email
+    }
+    console.log('YEEEEEI');
   }
-  console.log(newUser);
-});
+  });
+
+
 
 app.listen(3000,function(){
   console.log('server Started on Port 3000...');
